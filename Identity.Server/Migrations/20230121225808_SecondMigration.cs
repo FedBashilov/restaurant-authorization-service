d896b8e543408b14641eec6719fa2203303d9e53
fbashilov <fedbashilov@gmail.com>
2023-01-22 04:13:35 +0300

    Add db migrations

---
+++ Identity.Server/Migrations/20230121225808_SecondMigration.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Server.Migrations
+{
+    public partial class SecondMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Name",
+                table: "AspNetUsers",
+                newName: "FirstName");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "FirstName",
+                table: "AspNetUsers",
+                newName: "Name");
+        }
+    }
+}
