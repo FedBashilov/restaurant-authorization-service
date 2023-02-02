07ca03d26da62b72318fa37a1f6306b1f1a57e3b
fbashilov <fedbashilov@gmail.com>
2023-02-02 18:17:26 +0300

    update swagger

--- Identity.Server/Startup.cs
+++ Identity.Server/Startup.cs
@@ -12,6 +12,7 @@ namespace Web.Facade
     using Microsoft.EntityFrameworkCore;
     using Microsoft.Extensions.DependencyInjection;
     using Microsoft.IdentityModel.Tokens;
+    using Microsoft.OpenApi.Models;
     using Web.Facade.Models;
 
     public class Startup
@@ -27,7 +28,33 @@ namespace Web.Facade
         {
             services.AddAuthServices(this.Configuration);
 
-            services.AddSwaggerGen();
+            services.AddSwaggerGen(opt =>
+            {
+                opt.SwaggerDoc("v1", new OpenApiInfo { Title = "MyAPI", Version = "v1" });
+                opt.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    In = ParameterLocation.Header,
+                    Description = "Please enter token",
+                    Name = "Authorization",
+                    Type = SecuritySchemeType.Http,
+                    BearerFormat = "JWT",
+                    Scheme = "bearer",
+                });
+                opt.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    {
+                        new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer",
+                            },
+                        },
+                        new string[] { }
+                    },
+                });
+            });
 
             services.AddControllers();
         }
