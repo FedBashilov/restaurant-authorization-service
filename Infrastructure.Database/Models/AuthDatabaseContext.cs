3a152d3834f01a201f0b257810ace7e4a7b3d56d
fbashilov <fedbashilov@gmail.com>
2023-04-03 23:06:44 +0300

    Add adding roles

--- Infrastructure.Database/Models/AuthDatabaseContext.cs
+++ Infrastructure.Database/Models/AuthDatabaseContext.cs
@@ -3,6 +3,7 @@
 namespace Infrastructure.Database.Models
 {
     using Infrastructure.Core.Models;
+    using Microsoft.AspNetCore.Identity;
     using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
     using Microsoft.EntityFrameworkCore;
 
@@ -19,6 +20,27 @@ namespace Infrastructure.Database.Models
             modelBuilder.Entity<User>()
                 .Property(e => e.Name)
                 .HasMaxLength(250);
+
+            modelBuilder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = Core.Constants.UserRoles.Admin,
+                        NormalizedName = Core.Constants.UserRoles.Admin.ToUpper(),
+                    },
+                    new IdentityRole
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = Core.Constants.UserRoles.Cook,
+                        NormalizedName = Core.Constants.UserRoles.Cook.ToUpper(),
+                    },
+                    new IdentityRole
+                    {
+                        Id = Guid.NewGuid().ToString(),
+                        Name = Core.Constants.UserRoles.Client,
+                        NormalizedName = Core.Constants.UserRoles.Client.ToUpper(),
+                    });
         }
     }
 }
