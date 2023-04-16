a43c4be77f1b897f469b6335d63c9fdc7bf3bb44
fbashilov <fedbashilov@gmail.com>
2023-04-16 19:13:38 +0300

    Add EF migrations

--- Infrastructure.Database/Migrations/20230416161133_Migration1.Designer.cs
+++ Infrastructure.Database/Migrations/20230416161133_Migration1.Designer.cs
@@ -4,6 +4,7 @@ using Infrastructure.Database.Models;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Identity.Server.Migrations
 {
     [DbContext(typeof(AuthDatabaseContext))]
-    partial class AuthDatabaseContextModelSnapshot : ModelSnapshot
+    [Migration("20230416161133_Migration1")]
+    partial class Migration1
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Infrastructure.Database/Migrations/20230416161133_Migration1.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Identity.Server.Migrations
+{
+    public partial class Migration1 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "7d141565-db75-47cf-9291-bcdabe35edf9", "244265d4-34eb-41ba-bd94-69dc46492968", "admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "903469e1-65e2-44b4-b59d-c3d31d034ce9", "7154235b-c70b-4c41-9342-3535938f9cce", "client", "CLIENT" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "ccd7463f-8926-47a3-b618-545fd684a7e0", "c360bd5c-0055-47c7-900d-29c9e0378c10", "cook", "COOK" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "7d141565-db75-47cf-9291-bcdabe35edf9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "903469e1-65e2-44b4-b59d-c3d31d034ce9");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "ccd7463f-8926-47a3-b618-545fd684a7e0");
+        }
+    }
+}
